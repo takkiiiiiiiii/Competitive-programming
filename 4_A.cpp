9ebffb318c64d934844472b890b2f0a9a0e8e50f
takkiiiiiiiii25 <yudai2002brazil@icloud.com>
2021-05-07 21:37:30 +0900

    floatingpoint

---
+++ 4_A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a ;
+    int b ;
+    int d ;
+    int r ;
+    double f ;
+    cin >> a >> b ;
+    d = a / b ;
+    r = a % b ;
+    f =1.0 * a /  b ;
+    cout << fixed << setprecision(6) ;
+    cout << d <<" "<< r <<" "<< f << endl ;
+     return 0 ;
+}
+
