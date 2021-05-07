d84bd175f75ffd6f82746a1625fe2bf9ee74f768
takkiiiiiiiii25 <yudai2002brazil@icloud.com>
2021-05-07 21:38:28 +0900

    circle

---
+++ 4_B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    double r ;
+    double a ;
+    double c ;
+    cin >> r  ;
+    a = 3.141592653589 * pow(r , 2) ;
+    c = 2 * 3.141592653589 * r ;
+    cout << fixed << setprecision(6) ;
+    cout << a <<" "<< c << endl ;
+    return 0 ;
+}
+
